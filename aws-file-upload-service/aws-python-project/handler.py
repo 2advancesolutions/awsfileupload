dfd0d3dc346f4da737ba2db226b32d1a41119bf9
batman <2advancesolutions@gmail.com>
2023-12-29 13:46:22 -0600

    Add context parameter to s3_thumbnail_generator function and update serverless.yml

--- aws-file-upload-service/aws-python-project/handler.py
+++ aws-file-upload-service/aws-python-project/handler.py
@@ -7,7 +7,7 @@ import os
 s3 = boto3.client('s3')
 size = int(os.environ['THUMBNAIL_SIZE'])
 
-def s3_thumbnail_generator(event):
+def s3_thumbnail_generator(event, context):
 
     #parse event
     print("Event:::", event)
